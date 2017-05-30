2bb2c53ee865fec2c900fc3f30fbf1a17e238b79
Zachary Wander <zachary@techyteen.tk>
2017-05-30 18:42:56 -0400

    Add Fabric analytics

--- app/src/main/java/com/zacharee1/systemuituner/AppService.java
+++ app/src/main/java/com/zacharee1/systemuituner/AppService.java
@@ -4,6 +4,8 @@ import android.app.Application;
 import android.content.Intent;
 
 import com.zacharee1.systemuituner.services.ShutDownListen;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by Zacha on 5/1/2017.
@@ -13,6 +15,7 @@ public class AppService extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
 
         startService(new Intent(this, ShutDownListen.class));
     }
